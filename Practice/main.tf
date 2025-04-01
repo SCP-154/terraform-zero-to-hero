5bbef6959eb56a655878306c9a6066eb66eb9538
SCP-154 <57129563+SCP-154@users.noreply.github.com>
2025-04-01 10:18:13 +0800

    rm test

--- Practice/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "Linux" {
-  ami                     = "ami-071226ecf16aa7d96"
-  instance_type           = "t2.micro"
-  iam_instance_profile    = "arn:aws:iam::977099004432:instance-profile/EC2_ssm"
-}
